66f3d6cea74526d149271de4ce865f0e48fd6d3b
JWSScott777 <jwsscott777@gmail.com>
2023-10-15 14:31:06 -0400

    Changed small screen

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ import Footer from "./components/Footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen sm:w-full sm:h-screen sm:-0 flex-col bg-[#121212]">
       <Navbar />
-    <div className="container mt-24 mx-auto sm:w-full sm:h-screen sm:-0 px-12 py-4">
+    <div className="container mt-24 mx-auto px-12 py-4">
     <HeroSection />
     <AboutSection />
     <ProjectsSection />
