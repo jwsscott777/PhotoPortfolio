3c36a95035961b2786f0689e4706f89e4599d676
JWSScott777 <jwsscott777@gmail.com>
2023-10-15 14:14:51 -0400

    Fixed small screen

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-    <div className="container mt-24 mx-auto px-12 py-4">
+    <div className="container mt-24 mx-auto sm:w-full sm:h-screen sm:-0 px-12 py-4">
     <HeroSection />
     <AboutSection />
     <ProjectsSection />
     <EmailSection />
-    <Footer />
     </div>
+    <Footer />
     </main>
   )
 }
